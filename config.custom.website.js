cec01af15a33299ac2dd2ed4c08d215475f94079
Supri Hadianto <bintanEMAIL@gmail.com>
2022-06-24 16:01:24 +0700

    Update config.custom.website.js

--- config.custom.website.js
+++ config.custom.website.js
@@ -1,277 +1,286 @@
-const configCustomWebsite = {
-	custom_website: [{
-			website: "https://m.tribunnews.com",
-			domain_alias: "tribun",
-			element_remove: [
-				"#iklan_anchor",
-				".baca",
-				".op-interactive",
-				"#iklan_anchor",
-				".box__reserved.noreserved.mt20.mb20",
-				".box__reserved.mh10.nativeads",
-				".box__reserved.mh27.sticky",
-				".belanja-picks",
-				".cindeks",
-			],
-			element_white_list: [],
-			inject_element: "",
-			replace_single_words: [],
-		},
-		{
-			website: "https://www.indozone.id",
-			domain_alias: "dozon",
-			element_remove: [],
-			element_white_list: [],
-			inject_element: "",
-			replace_single_words: [],
-		},
-		{
-			website: "https://www.rottentomatoes.com",
-			domain_alias: "rotten",
-			element_remove: [],
-			element_white_list: ["script"],
-			inject_element: "",
-			replace_single_words: [],
-		},
-		{
-			website: "https://www.bagas31.info",
-			domain_alias: "bagas",
-			element_remove: [
-				".coc-gems",
-				".hh-gen",
-				".blogposts-inner:nth-child(1)",
-				"#text-9",
-				"#custom_html-2.widget_custom_html",
-				"#custom_html-3.widget_custom_html",
-				"#custom_html-4.widget_custom_html",
-				"#custom_html-6.widget_custom_html",
-				"#custom_html-7.widget_custom_html",
-				".coroabiscuy",
-				".copyright",
-				"#logo",
-			],
-			element_white_list: [],
-			inject_element: "bagas31.js",
-			replace_single_words: [{
-				element: "span[itemprop='description'] > p > strong",
-				search_word: "BAGAS31",
-				replace_with: "Gogon",
-			}, ],
-			replace_global_words: [{
-				search_word: "BAGAS31",
-				replace_with: "MY WEBSITE",
-			}, ],
-		},
-		{
-			website: "https://katadata.co.id",
-			domain_alias: "catatankatadata",
-			element_remove: [],
-			element_white_list: [],
-			inject_element: "",
-			replace_single_words: [],
-		},
-		{
-			website: "https://coderprog.com",
-			domain_alias: "coderprog",
-			element_remove: [],
-			element_white_list: [],
-			inject_element: "",
-			replace_single_words: [],
-		},
-		{
-			website: "https://www.nesabamedia.com",
-			domain_alias: "naseba",
-			element_remove: [],
-			element_white_list: [],
-			inject_element: "",
-			replace_single_words: [],
-		},
-		{
-			website: "https://mifirm.net",
-			domain_alias: "catatanmifirm",
-			element_remove: [],
-			element_white_list: [],
-			inject_element: "",
-			replace_single_words: [],
-		},
-		{
-			website: "https://alltubedownload.net",
-			domain_alias: "tubedownload",
-			element_remove: [],
-			element_white_list: ["form"],
-			inject_element: "alltubedownload.js",
-			replace_single_words: [],
-		},
-		{
-			website: "https://www.masdayat.net",
-			domain_alias: "masdayat",
-			element_remove: [],
-			element_white_list: [],
-			inject_element: "",
-			replace_single_words: [],
-		},
-		{
-			website: "https://www.coindesk.com",
-			domain_alias: "codesk",
-			element_remove: [],
-			element_white_list: [],
-			inject_element: "",
-			replace_single_words: [],
-		},
-		{
-			website: "https://brainly.co.id",
-			domain_alias: "catatanbrain",
-			element_remove: [
-				"header",
-				".brn-header-container",
-				".brn-qpage-next-question-box__actions",
-				".brn-qpage-next-question-box__options",
-				".brn-qpage-next-question-box__comments",
-				".js-react-below-answers",
-				".brn-qpage-next-answer-box__comments",
-				".brn-qpage-next-answer-box__community-banner",
-				".brn-qpage-next-answer-box__actions",
-				".brn-placeholder__animation-box",
-				".brn-placeholder__animation",
-				".brn-text-block-shade.brn-qpage-next-answer-box-author__attributes-placeholder",
-				"#new-ad-placeholder-question-mobile",
-				"#new-ad-placeholder-question-desktop",
-				".brn-ads-box",
-				".brn-ads-mobile-container",
-				".js-react-newest-questions",
-				"footer"
-			],
-			element_white_list: [],
-			inject_element: "",
-			replace_single_words: [],
-		},
-		{
-			website: "https://brainly.com",
-			domain_alias: "catatanbrainen",
-			element_remove: [
-				"header",
-				".brn-header-container",
-				".brn-qpage-next-question-box__actions",
-				".brn-qpage-next-question-box__options",
-				".brn-qpage-next-question-box__comments",
-				".js-react-below-answers",
-				".brn-qpage-next-answer-box__comments",
-				".brn-qpage-next-answer-box__community-banner",
-				".brn-qpage-next-answer-box__actions",
-				".brn-placeholder__animation-box",
-				".brn-placeholder__animation",
-				".brn-text-block-shade.brn-qpage-next-answer-box-author__attributes-placeholder",
-				"#new-ad-placeholder-question-mobile",
-				"#new-ad-placeholder-question-desktop",
-				".brn-ads-box",
-				".brn-ads-mobile-container",
-				".js-react-newest-questions",
-				"footer"
-			],
-			element_white_list: [],
-			inject_element: "",
-			replace_single_words: [],
-		},
-		{
-			website: "https://askubuntu.com",
-			domain_alias: "catatanaskubuntu",
-			element_remove: [],
-			element_white_list: [],
-			inject_element: "",
-			replace_single_words: [],
-		},
-		{
-			website: "https://stackoverflow.com",
-			domain_alias: "catatanoverflow",
-			element_remove: [],
-			element_white_list: [],
-			inject_element: "",
-			replace_single_words: [],
-		},
-		{
-			website: "https://samfw.com",
-			domain_alias: "samfw",
-			element_remove: [],
-			element_white_list: [],
-			inject_element: "",
-			replace_single_words: [],
-		},
-		{
-			website: "https://reviews.tn",
-			domain_alias: "reviewstn",
-			element_remove: [],
-			element_white_list: [],
-			inject_element: "",
-			replace_single_words: [],
-		},
-		{
-			website: "https://www.imei.info",
-			domain_alias: "imeiinfo",
-			element_remove: [],
-			element_white_list: [],
-			inject_element: "",
-			replace_single_words: [],
-		},
-		{
-			website: "https://www.cdc.gov",
-			domain_alias: "cdcgov",
-			element_remove: [],
-			element_white_list: [],
-			inject_element: "",
-			replace_single_words: [],
-		},
-		{
-			website: "https://www.gsmarena.com",
-			domain_alias: "gsmarena",
-			element_remove: [],
-			element_white_list: [],
-			inject_element: "",
-			replace_single_words: [],
-		},
-		{
-			website: "https://www.sammobile.com",
-			domain_alias: "sammobile",
-			element_remove: [],
-			element_white_list: [],
-			inject_element: "",
-			replace_single_words: [],
-		},
-		{
-			website: "https://www.pojokseni.com",
-			domain_alias: "catatanpojok",
-			element_remove: [
-				".top-ads",
-				"footer",
-				".backtotop",
-				".center-ads1",
-				".center-ads2",
-				".bottom-ads",
-				".iklan-bawah-postingan",
-				"sidebar-ads",
-				"#floatads"
-			],
-			element_white_list: [],
-			inject_element: "",
-			replace_single_words: [],
-		},
-		{
-			website: "https://comicbook.com",
-			domain_alias: "catatancomicbook",
-			element_remove: [
-				"#header",
-				"#UserMessages",
-				"#omni-skybox-plus-top",
-				"#mobile-omni-skybox-plus-top",
-				"footer",
-				"#slotted-content",
-				"#newsletterModal",
-				".js-videoEmbed"
-			],
-			element_white_list: ["header.article-header"],
-			inject_element: "",
-			replace_single_words: [],
-		},
-	],
-};
-
-module.exports = configCustomWebsite;
+{
+  "custom_website": [
+    {
+      "website": "https://m.tribunnews.com",
+      "domain_alias": "tribun",
+      "element_remove": [
+        "#iklan_anchor",
+        ".baca",
+        ".op-interactive",
+        "#iklan_anchor",
+        ".box__reserved.noreserved.mt20.mb20",
+        ".box__reserved.mh10.nativeads",
+        ".box__reserved.mh27.sticky",
+        ".belanja-picks",
+        ".cindeks"
+      ],
+      "element_white_list": [],
+      "inject_element": "",
+      "replace_single_words": []
+    },
+    {
+      "website": "https://www.indozone.id",
+      "domain_alias": "dozon",
+      "element_remove": [],
+      "element_white_list": [],
+      "inject_element": "",
+      "replace_single_words": []
+    },
+    {
+      "website": "https://www.rottentomatoes.com",
+      "domain_alias": "rotten",
+      "element_remove": [],
+      "element_white_list": [
+        "script"
+      ],
+      "inject_element": "",
+      "replace_single_words": []
+    },
+    {
+      "website": "https://www.bagas31.info",
+      "domain_alias": "bagas",
+      "element_remove": [
+        ".coc-gems",
+        ".hh-gen",
+        ".blogposts-inner:nth-child(1)",
+        "#text-9",
+        "#custom_html-2.widget_custom_html",
+        "#custom_html-3.widget_custom_html",
+        "#custom_html-4.widget_custom_html",
+        "#custom_html-6.widget_custom_html",
+        "#custom_html-7.widget_custom_html",
+        ".coroabiscuy",
+        ".copyright",
+        "#logo"
+      ],
+      "element_white_list": [],
+      "inject_element": "bagas31.js",
+      "replace_single_words": [
+        {
+          "element": "span[itemprop='description'] > p > strong",
+          "search_word": "BAGAS31",
+          "replace_with": "Gogon"
+        }
+      ],
+      "replace_global_words": [
+        {
+          "search_word": "BAGAS31",
+          "replace_with": "MY WEBSITE"
+        }
+      ]
+    },
+    {
+      "website": "https://katadata.co.id",
+      "domain_alias": "catatankatadata",
+      "element_remove": [],
+      "element_white_list": [],
+      "inject_element": "",
+      "replace_single_words": []
+    },
+    {
+      "website": "https://coderprog.com",
+      "domain_alias": "coderprog",
+      "element_remove": [],
+      "element_white_list": [],
+      "inject_element": "",
+      "replace_single_words": []
+    },
+    {
+      "website": "https://www.nesabamedia.com",
+      "domain_alias": "naseba",
+      "element_remove": [],
+      "element_white_list": [],
+      "inject_element": "",
+      "replace_single_words": []
+    },
+    {
+      "website": "https://mifirm.net",
+      "domain_alias": "catatanmifirm",
+      "element_remove": [],
+      "element_white_list": [],
+      "inject_element": "",
+      "replace_single_words": []
+    },
+    {
+      "website": "https://alltubedownload.net",
+      "domain_alias": "tubedownload",
+      "element_remove": [],
+      "element_white_list": [
+        "form"
+      ],
+      "inject_element": "alltubedownload.js",
+      "replace_single_words": []
+    },
+    {
+      "website": "https://www.masdayat.net",
+      "domain_alias": "masdayat",
+      "element_remove": [],
+      "element_white_list": [],
+      "inject_element": "",
+      "replace_single_words": []
+    },
+    {
+      "website": "https://www.coindesk.com",
+      "domain_alias": "codesk",
+      "element_remove": [],
+      "element_white_list": [],
+      "inject_element": "",
+      "replace_single_words": []
+    },
+    {
+      "website": "https://brainly.co.id",
+      "domain_alias": "catatanbrain",
+      "element_remove": [
+        "header",
+        ".brn-header-container",
+        ".brn-qpage-next-question-box__actions",
+        ".brn-qpage-next-question-box__options",
+        ".brn-qpage-next-question-box__comments",
+        ".js-react-below-answers",
+        ".brn-qpage-next-answer-box__comments",
+        ".brn-qpage-next-answer-box__community-banner",
+        ".brn-qpage-next-answer-box__actions",
+        ".brn-placeholder__animation-box",
+        ".brn-placeholder__animation",
+        ".brn-text-block-shade.brn-qpage-next-answer-box-author__attributes-placeholder",
+        "#new-ad-placeholder-question-mobile",
+        "#new-ad-placeholder-question-desktop",
+        ".brn-ads-box",
+        ".brn-ads-mobile-container",
+        ".js-react-newest-questions",
+        "footer"
+      ],
+      "element_white_list": [],
+      "inject_element": "",
+      "replace_single_words": []
+    },
+    {
+      "website": "https://brainly.com",
+      "domain_alias": "catatanbrainen",
+      "element_remove": [
+        "header",
+        ".brn-header-container",
+        ".brn-qpage-next-question-box__actions",
+        ".brn-qpage-next-question-box__options",
+        ".brn-qpage-next-question-box__comments",
+        ".js-react-below-answers",
+        ".brn-qpage-next-answer-box__comments",
+        ".brn-qpage-next-answer-box__community-banner",
+        ".brn-qpage-next-answer-box__actions",
+        ".brn-placeholder__animation-box",
+        ".brn-placeholder__animation",
+        ".brn-text-block-shade.brn-qpage-next-answer-box-author__attributes-placeholder",
+        "#new-ad-placeholder-question-mobile",
+        "#new-ad-placeholder-question-desktop",
+        ".brn-ads-box",
+        ".brn-ads-mobile-container",
+        ".js-react-newest-questions",
+        "footer"
+      ],
+      "element_white_list": [],
+      "inject_element": "",
+      "replace_single_words": []
+    },
+    {
+      "website": "https://askubuntu.com",
+      "domain_alias": "catatanaskubuntu",
+      "element_remove": [],
+      "element_white_list": [],
+      "inject_element": "",
+      "replace_single_words": []
+    },
+    {
+      "website": "https://stackoverflow.com",
+      "domain_alias": "catatanoverflow",
+      "element_remove": [],
+      "element_white_list": [],
+      "inject_element": "",
+      "replace_single_words": []
+    },
+    {
+      "website": "https://samfw.com",
+      "domain_alias": "samfw",
+      "element_remove": [],
+      "element_white_list": [],
+      "inject_element": "",
+      "replace_single_words": []
+    },
+    {
+      "website": "https://reviews.tn",
+      "domain_alias": "reviewstn",
+      "element_remove": [],
+      "element_white_list": [],
+      "inject_element": "",
+      "replace_single_words": []
+    },
+    {
+      "website": "https://www.imei.info",
+      "domain_alias": "imeiinfo",
+      "element_remove": [],
+      "element_white_list": [],
+      "inject_element": "",
+      "replace_single_words": []
+    },
+    {
+      "website": "https://www.cdc.gov",
+      "domain_alias": "cdcgov",
+      "element_remove": [],
+      "element_white_list": [],
+      "inject_element": "",
+      "replace_single_words": []
+    },
+    {
+      "website": "https://www.gsmarena.com",
+      "domain_alias": "gsmarena",
+      "element_remove": [],
+      "element_white_list": [],
+      "inject_element": "",
+      "replace_single_words": []
+    },
+    {
+      "website": "https://www.sammobile.com",
+      "domain_alias": "sammobile",
+      "element_remove": [],
+      "element_white_list": [],
+      "inject_element": "",
+      "replace_single_words": []
+    },
+    {
+      "website": "https://www.pojokseni.com",
+      "domain_alias": "catatanpojok",
+      "element_remove": [
+        ".top-ads",
+        "footer",
+        ".backtotop",
+        ".center-ads1",
+        ".center-ads2",
+        ".bottom-ads",
+        ".iklan-bawah-postingan",
+        "sidebar-ads",
+        "#floatads"
+      ],
+      "element_white_list": [],
+      "inject_element": "",
+      "replace_single_words": []
+    },
+    {
+      "website": "https://comicbook.com",
+      "domain_alias": "catatancomicbook",
+      "element_remove": [
+        "#header",
+        "#UserMessages",
+        "#omni-skybox-plus-top",
+        "#mobile-omni-skybox-plus-top",
+        "footer",
+        "#slotted-content",
+        "#newsletterModal",
+        ".js-videoEmbed"
+      ],
+      "element_white_list": [
+        "header.article-header"
+      ],
+      "inject_element": "",
+      "replace_single_words": []
+    }
+  ]
+}
