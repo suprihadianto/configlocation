d0419ff1d137a08f06f2b66f2f1f52010b310312
Supri Hadianto <bintanEMAIL@gmail.com>
2022-06-24 15:37:04 +0700

    Add files via upload

---
+++ config.custom.head.js
@@ -0,0 +1,186 @@
+const configCustomHead = {
+  custom_head: [
+    {
+      name: "meta",
+      data: "",
+      position: "start",
+      property: [
+        {
+          attribute: "name",
+          value: "google-site-verification",
+        },
+        {
+          attribute: "content",
+          value: "HeoaF1EWrYhgXsRwL41FYLo0KeanX9GhJppBej-CkYo",
+        },
+      ],
+    },
+    {
+      name: "meta",
+      data: "",
+      position: "start",
+      property: [
+        {
+          attribute: "name",
+          value: "viewport",
+        },
+        {
+          attribute: "content",
+          value:
+            "width=device-width, initial-scale=1.0, user-scalable=1.0, minimum-scale=1.0, maximum-scale=5.0",
+        },
+      ],
+    },
+    {
+      name: "meta",
+      data: "",
+      position: "start",
+      property: [
+        {
+          attribute: "name",
+          value: "description",
+        },
+        {
+          attribute: "content",
+          value: "${descriptionPost}",
+        },
+      ],
+    },
+    {
+      name: "meta",
+      data: "",
+      position: "start",
+      property: [
+        {
+          attribute: "charset",
+          value: "UTF-8",
+        },
+      ],
+    },
+    {
+      name: "meta",
+      data: "",
+      position: "start",
+      property: [
+        {
+          attribute: "property",
+          value: "og:type",
+        },
+        {
+          attribute: "content",
+          value: "article",
+        },
+      ],
+    },
+    {
+      name: "meta",
+      data: "",
+      position: "start",
+      property: [
+        {
+          attribute: "property",
+          value: "og:title",
+        },
+        {
+          attribute: "content",
+          value: "${titlePost}",
+        },
+      ],
+    },
+    {
+      name: "meta",
+      data: "",
+      position: "start",
+      property: [
+        {
+          attribute: "property",
+          value: "og:description",
+        },
+        {
+          attribute: "content",
+          value: "${descriptionPost}",
+        },
+      ],
+    },
+    {
+      name: "meta",
+      data: "",
+      position: "start",
+      property: [
+        {
+          attribute: "property",
+          value: "og:url",
+        },
+        {
+          attribute: "content",
+          value: "${urlPost}",
+        },
+      ],
+    },
+    {
+      name: "meta",
+      data: "",
+      position: "start",
+      property: [
+        {
+          attribute: "property",
+          value: "og:site_name",
+        },
+        {
+          attribute: "content",
+          value: "${nameWeb}",
+        },
+      ],
+    },
+    {
+      name: "meta",
+      data: "",
+      position: "start",
+      property: [
+        {
+          attribute: "property",
+          value: "article:published_time",
+        },
+        {
+          attribute: "content",
+          value: "${timePublish}",
+        },
+      ],
+    },
+    {
+      name: "meta",
+      data: "",
+      position: "start",
+      property: [
+        {
+          attribute: "property",
+          value: "article:author",
+        },
+        {
+          attribute: "content",
+          value: "${authorPost}",
+        },
+      ],
+    },
+    // {
+    // 	name: "script",
+    // 	data: "",
+    // 	position: "start",
+    // 	property: [{
+    // 			attribute: "async",
+    // 			value: "async"
+    // 		},
+    // 		{
+    // 			attribute: "src",
+    // 			value: "https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"
+    // 		},
+    // 		{
+    // 			attribute: "data-ad-client",
+    // 			value: "ca-pub-286cxc017040005690454545"
+    // 		}
+    // 	]
+    // }
+  ],
+};
+
+module.exports = configCustomHead;
---
+++ config.general.js
@@ -0,0 +1,27 @@
+const config = {
+	hostname: "http://localhost:3000",
+	sitename: "sebuahcatatan",
+	sitedescription: "",
+	author: "SEBUAHCATATAN",
+	homepage: {
+		website: "https://alltubedownload.net",
+		domain_alias: "",
+		element_remove: [],
+		element_white_list: ["form"],
+		inject_element: "",
+		replace_single_words: [],
+	},
+	sitemap_name: "sitemap.xml",
+	remove_comment_html: false,
+	element_remove: [
+		"script",
+		"noscript",
+		"meta",
+		"iframe",
+		"embed",
+		"form",
+		"ins",
+	]
+};
+
+module.exports = config;
